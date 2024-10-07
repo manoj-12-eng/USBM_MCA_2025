0089a5e8a5b79a5e607cf615b9e3759d85908da6
manoj-12-eng <kumarm55035@gmail.com>
2024-10-07 12:02:42 +0530

    task

---
+++ frontend/javascript/class1/class1/data.js
@@ -0,0 +1,45 @@
+
+//undefind data
+
+
+let data;         
+console.log(typeof data);
+console.log(data)
+  
+
+//number
+//let age=22;
+//console.log("my age is :",age);
+//console.log(typeof age);
+let pie =3.14;
+console.log("the value of pie is :",pie);
+console.log(typeof pie);
+
+
+let num1=4;
+let num2=9;
+let sum=num1+num2;
+console.log(sum);
+
+let num3=4;
+let num4=9;
+let s=num3*num4;
+console.log(s);
+
+let num6=4;
+let num7=9;
+let m=num1-num2;
+console.log(m);
+
+let num8="anjan";
+let num5=10;
+let u=num1/num2;
+console.log(u);
+
+let name='manoj';
+let age=24;
+let city='dhenkanal';
+console.log(`my age is:${age} \n my name is:${name} \n my city is :${city}`);
+
+
+
